c77aa5390de10d99d06d398b117e48459566ee68
Aldana Delicia <noealdana2833@gmail.com>
2022-09-22 15:20:23 -0300

    cambios

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let option;
-const tasks = [
+let tasks = [
     {
         id: 1,
         day:1,
-        task: 'ir a desayunar',
+        task: 'ir a comer',
         state: 'pendiente'
     },
+  
     {
         id: 2,
         day:2,
-        task: ' ir a comer',
+        task: 'juntada familiar',
         state: 'pendiente'
     },
 ];
@@ -30,19 +31,25 @@ const states = [
 ];
   
 while(option !== 0) {
-    option = Number (prompt('ingrese una opcion; \n1. agregar tarea \n2. ver tareas \n0. salir '));
+    option = Number (prompt('ingrese una opcion; \n1. agregar tarea \n2. ver tareas \n3. borrar tarea \n0. salir '));
     switch (option) {
         case 1:
             const day = Number (prompt('ingrese el dia de la semana por la tarea que desea crear:\n1. lunes \n2. martes \n3. miercoles \n.4 jueves \n5. viernes \n6. sabado \n7. domingo'));
             const task = prompt ('ingrese la tarea:');
             const state = Number(prompt('ingrese el estado de la tarea: \n1. En proceso \n2. Pendiente \n3. Finalizado'));
-            const id = getLastID + 1;
-            createtask (id, day, task, state);
+            const creationId = getLastID + 1;
+            createtask (creationId, day, task, state);
             
             break; 
 
         case 2:
             getAllTasks ();
+
+            break;
+
+        case 3:
+            let deleteId = Number(prompt('ingrese el id de la tarea a eliminar:'));
+            deletetask();
 
             break;
 
@@ -72,6 +79,32 @@ function getAllTasks (){
     tasks.forEach((task) => console.log(task.id + " - " + day[task.day - 1] + " - " + task.task + " - " + task.state));
 }
 
+function deletetask (id) {
+    tasks = tasks.filter (task => task.id != id);
+}
+
 function getLastID (){
     return tasks.length;
 }
+
+
+const dias = ['lunes', 'martes', 'miercoles','jueves','viernes','sabado','domingo'];
+let select = document.createElement("select");
+for (let index = 0; index < dias.length; index++) {
+    select.innerHTML +=  `<option value='${index}'>${dias[index]}</option>`;
+}
+
+select.addEventListener('change', function (e) {
+    const h3 = document.createElement('h3');
+    h3.innerHTML = dias[e.target.value];
+    document.body.appendChild(h3);
+})
+document.body.appendChild(select);
+
+let usuario = localStorage.getItem('nombre');
+if (usuario == null) {
+    localStorage.setItem('nombre', prompt('INGRESAR NOMBRE'));
+} else {
+    alert('EL NOMBRE ES ' + usuario);
+}
+
