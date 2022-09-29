c7fcded10f538fbdb7c31e3caa877917ac9cbf73
Aldana Delicia <noealdana2833@gmail.com>
2022-09-29 15:57:08 -0300

    cambios

--- index.js
+++ index.js
@@ -29,12 +29,29 @@ const states = [
     'Pendiente',
     'finalizado',
 ];
+
+const dias = ['lunes', 'martes', 'miercoles','jueves','viernes','sabado','domingo'];
+let select = document.createElement("select");
+for (let index = 0; index < dias.length; index++) {
+    select.innerHTML +=  `<option value='${index}'>${dias[index]}</option>`;
+}
+
+select.addEventListener('change', function (e) {
+    const h3 = document.createElement('h3');
+    h3.innerHTML = dias[e.target.value];
+    document.body.appendChild(h3);
+})
+document.body.appendChild(select);
+ 
+
+
   
 while(option !== 0) {
-    option = Number (prompt('ingrese una opcion; \n1. agregar tarea \n2. ver tareas \n3. borrar tarea \n0. salir '));
+
+    option = Number (prompt('ingrese una opcion; \n1. agregar tarea \n2. ver tareas \n0. salir '));
     switch (option) {
         case 1:
-            const day = Number (prompt('ingrese el dia de la semana por la tarea que desea crear:\n1. lunes \n2. martes \n3. miercoles \n.4 jueves \n5. viernes \n6. sabado \n7. domingo'));
+            const day = Number (prompt('ingrese el dia de la semana por la tarea que desea crear:\n1. lunes \n2. martes \n3. miercoles \n4. jueves \n5. viernes \n6. sabado \n7. domingo'));
             const task = prompt ('ingrese la tarea:');
             const state = Number(prompt('ingrese el estado de la tarea: \n1. En proceso \n2. Pendiente \n3. Finalizado'));
             const creationId = getLastID + 1;
@@ -47,12 +64,7 @@ while(option !== 0) {
 
             break;
 
-        case 3:
-            let deleteId = Number(prompt('ingrese el id de la tarea a eliminar:'));
-            deletetask();
-
-            break;
-
+       
         case 0: 
              alert('gracias,vuelva pronto');
             break;
@@ -79,32 +91,19 @@ function getAllTasks (){
     tasks.forEach((task) => console.log(task.id + " - " + day[task.day - 1] + " - " + task.task + " - " + task.state));
 }
 
-function deletetask (id) {
-    tasks = tasks.filter (task => task.id != id);
-}
+
 
 function getLastID (){
     return tasks.length;
 }
 
 
-const dias = ['lunes', 'martes', 'miercoles','jueves','viernes','sabado','domingo'];
-let select = document.createElement("select");
-for (let index = 0; index < dias.length; index++) {
-    select.innerHTML +=  `<option value='${index}'>${dias[index]}</option>`;
-}
-
-select.addEventListener('change', function (e) {
-    const h3 = document.createElement('h3');
-    h3.innerHTML = dias[e.target.value];
-    document.body.appendChild(h3);
-})
-document.body.appendChild(select);
 
 let usuario = localStorage.getItem('nombre');
 if (usuario == null) {
     localStorage.setItem('nombre', prompt('INGRESAR NOMBRE'));
 } else {
-    alert('EL NOMBRE ES ' + usuario);
+    console.log('Datos diarios de'+ " " + usuario);
 }
+
 
